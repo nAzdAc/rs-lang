8ca6903f4f28ad4c6b5f2cacad0a1d9c9a0cea6b
Nicolay-kr <nicolay.krischenovich@gmail.com>
2021-03-26 09:08:12 +0300

    feat(signiIn): add name field

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import React, { useState, useCallback } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
+      {"Copyright © "}
       <Link color="inherit" href="https://material-ui.com/">
         RS Lang
-      </Link>{' '}
+      </Link>{" "}
       {new Date().getFullYear()}
-      {'.'}
+      {"."}
     </Typography>
   );
 }
@@ -29,16 +29,16 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: '100%', // Fix IE 11 issue.
+    width: "100%", // Fix IE 11 issue.
     marginTop: theme.spacing(1),
   },
   submit: {
@@ -48,6 +48,45 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignIn() {
   const classes = useStyles();
+  const [isAccount, setIsAccount] = useState(true);
+  const [form, setForm] = useState({
+    'name': '',
+    'email': '',
+    'password': ''
+  });
+  const handleClick = () => {
+    setIsAccount(false);
+  };
+  const handleFrormChange = (e) =>{
+    setForm({...form,[e.target.name]:e.target.value})
+    
+  }
+  const handlerSubmit = (e) =>{
+    setForm({...form,[e.target.name]:e.target.value})
+    
+  }
+  let nameField;
+  if (!isAccount) {
+    nameField = (
+      <TextField
+        variant="outlined"
+        margin="normal"
+        required
+        fullWidth
+        name="name"
+        label="Name"
+        type="text"
+        id="name"
+        autoComplete="name"
+        value={form.name}
+        onChange={handleFrormChange}
+      />
+    );
+  } else {
+    nameField = null;
+  }
+  
+ 
 
   return (
     <Container component="main" maxWidth="xs">
@@ -59,7 +98,8 @@ export default function SignIn() {
         <Typography component="h1" variant="h5">
           Sign in
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={handlerSubmit}>
+          {nameField}
           <TextField
             variant="outlined"
             margin="normal"
@@ -70,6 +110,8 @@ export default function SignIn() {
             name="email"
             autoComplete="email"
             autoFocus
+            value={form.email}
+            onChange={handleFrormChange}
           />
           <TextField
             variant="outlined"
@@ -81,7 +123,10 @@ export default function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
+            value={form.password}
+            onChange={handleFrormChange}
           />
+
           <FormControlLabel
             control={<Checkbox value="remember" color="primary" />}
             label="Remember me"
@@ -95,7 +140,7 @@ export default function SignIn() {
           >
             Sign In
           </Button>
-          <Grid container>
+          <Grid container onClick={handleClick}>
             <Grid item xs>
               <Link href="#" variant="body2">
                 Forgot password?
