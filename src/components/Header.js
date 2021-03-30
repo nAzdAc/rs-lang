37cd824d7535caaca3377d5c86a9730b4e6182dd
Ivan Shvets <shvets.ivan@gmail.com>
2021-03-30 11:59:40 +0300

    add: basic redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,25 @@
-import { HeaderStyled } from '../styles/Header.styled'
+import { HeaderStyled } from '../styles/Header.styled';
 import Menu from './Menu';
 import { Link } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
+import { changeTest } from '../store/testSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    dispatch(changeTest('logo'));
+    console.log('click on main logo');
+  };
+
   return (
-    <HeaderStyled>
-      <Link to='/'>
+    <HeaderStyled onClick={onClick}>
+      <Link to="/">
         <p>Rs Lang</p>
       </Link>
-      <Menu/>
+      <Menu />
     </HeaderStyled>
-  )
-} 
+  );
+};
 
 export default Header;
