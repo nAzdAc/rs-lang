4ac5804f01f097f0a23a23776b29d3bb5df351f2
Aleksey Goravskiy <32121103+AGoravskiy@users.noreply.github.com>
2021-04-01 23:53:39 +0300

    feat: add mane page

--- src/App.js
+++ src/App.js
@@ -8,8 +8,9 @@ import { GamesPage } from './pages/GamesPage';
 import { SprintPage } from './pages/SprintPage';
 import { frontRoutes } from './utils/frontRoutes';
 import { Footer } from './components/Footer';
-import SignUpPage from './pages/SignUpPage'
-import SignInPage from './pages/SignInPage'
+import SignUpPage from './pages/SignUpPage';
+import SignInPage from './pages/SignInPage';
+import ManePage from './pages/MainPage';
 
 const RouteComponent = ({ text }) => <div>{text}</div>;
 
@@ -19,44 +20,47 @@ function App() {
 			<div className="App">
 				<Header />
 				<Switch>
-					<Route path="/book">
-						<RouteComponent text="Book" />
-					</Route>
-					{bookLinks.map((link, index) => (
-						<Route path={link.to} key={index}>
-							<RouteComponent text={link.text} />
-						</Route>
-					))}
-					<Route path="/games">
-						<GamesPage />
-					</Route>
-						<Route path={frontRoutes.savanna}>
-							<SprintPage />
-						</Route>
-						<Route path={frontRoutes.audio}>
-							<SprintPage />
-						</Route>
-						<Route path={frontRoutes.sprint}>
-							<SprintPage />
-						</Route>
-						<Route path={frontRoutes.match}>
-							<SprintPage />
-						</Route>
-					<Route path="/dictionary">
-						<RouteComponent text="Словарь" />
-					</Route>
-					<Route path="/stats">
-						<RouteComponent text="Статистика" />
-					</Route>
-					<Route path="/settings">
-						<SettingsPage />
-					</Route>
-					<Route path="/signin">
-						<SignInPage/>
-					</Route>
-					<Route path="/signup">
-						<SignUpPage/>
-					</Route>
+          <Route exact path="/">
+            <ManePage/>
+          </Route>
+          <Route path="/book">
+            <RouteComponent text="Book" />
+          </Route>
+          {bookLinks.map((link, index) => (
+            <Route path={link.to} key={index}>
+              <RouteComponent text={link.text} />
+            </Route>
+          ))}
+          <Route path="/games">
+            <GamesPage />
+          </Route>
+            <Route path={frontRoutes.savanna}>
+              <SprintPage />
+            </Route>
+            <Route path={frontRoutes.audio}>
+              <SprintPage />
+            </Route>
+            <Route path={frontRoutes.sprint}>
+              <SprintPage />
+            </Route>
+            <Route path={frontRoutes.match}>
+              <SprintPage />
+            </Route>
+          <Route path="/dictionary">
+            <RouteComponent text="Словарь" />
+          </Route>
+          <Route path="/stats">
+            <RouteComponent text="Статистика" />
+          </Route>
+          <Route path="/settings">
+            <SettingsPage />
+          </Route>
+          <Route path="/signin">
+            <SignInPage/>
+          </Route>
+          <Route path="/signup">
+            <SignUpPage/>
+          </Route>
 				</Switch>
         <Footer/>
 			</div>
